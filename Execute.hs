a258b6195710f774814f3e955178e0f9e8e15a5f
Andy Bovett <abovett@xemaps.com>
2022-05-06 18:30:57 +0100

    Use Either monad for errors in step; factor out getVal.

--- Execute.hs
+++ Execute.hs
@@ -9,7 +9,8 @@ import Control.Monad.IO.Class
 import Control.Monad.Trans.State
 import qualified Data.Map as Map
 
-import Common ( Address
+import Common ( LineNo
+              , Address
               , Instruction (..)
               , OpCode (..)
               , Operand (..)
@@ -38,51 +39,56 @@ runProgram pr dv = do
 
 
 -- Main execution "loop"
+-- TODO can this use a monad?
 execute :: Computer -> IO ()
 execute comp = do
-  let (comp', output) = step comp
-  putStr output
-  if halted comp'
-    then return ()
-    else execute comp'
+  case step comp of
+    Left err -> putStrLn err
+    Right (comp', output) -> do
+      putStr output
+      if halted comp'
+        then return ()
+        else execute comp'
 
 -- TODO add line number to error references
 
 -- Execute a single step/cycle
-step :: Computer -> (Computer, String)
-step comp
-  | pc comp >= (toInteger . length . program $ comp) =
-    ( comp{ halted = True }
-    , "PC OUT OF RANGE: " ++ (show $ pc comp) ++ "\n"
-    )
+step :: Computer -> Either String (Computer, String)
+step comp = do
+  pcv <- if pc comp >= (toInteger . length . program $ comp)
+         then Left $ "PC OUT OF RANGE: " ++ (show $ pc comp) ++ "\n"
+         else Right . fromIntegral . pc $ comp
 
-  -- TODO can any common parts be factored out?
-  | otherwise =
-      let pcv = fromIntegral $ pc comp
-          Instruction lineNo opCode operand = program comp !! pcv
-      in case opCode of
-        HALT -> ( comp{ halted = True }, "\nHALT\n" )
+  let Instruction lineNo opCode operand = program comp !! pcv
 
-        LINE -> ( comp{ pc = pc comp + 1 } , "\n" )
+  case opCode of
+    HALT -> Right ( comp{ halted = True }, "\nHALT\n" )
 
-        PRINT -> let TextOperand s = operand
-                 in ( comp{ pc = pc comp + 1 }, s )
+    LINE -> Right ( comp{ pc = pc comp + 1 } , "\n" )
 
-        LOAD -> let ValueOperand v = operand
-                    val = case v of
-                            Left n -> Right n
-                            Right s -> case Map.lookup s $ ram comp of
-                              Just n -> Right n
-                              Nothing -> Left $ "\nERROR: Unknown variable: '"
-                                ++ s ++ "' at line " ++ show lineNo ++ "\n"
-                 in case val of
-                      Left err -> ( comp { halted = True }, err )
-                      Right n -> ( comp { pc = pc comp + 1, acc = n }, "")
+    PRINT ->
+      let TextOperand s = operand
+      in Right ( comp{ pc = pc comp + 1 }, s )
 
-        STORE -> let SymbolOperand s = operand
-                     comp'=  comp{ pc = pc comp + 1
-                                 , ram = Map.insert s (acc comp) (ram comp) }
-                 in ( comp', "" )
-        _ -> let comp' = comp{ pc = pc comp + 1 }
-                 output = "step: PC = " ++  (show $ pc comp') ++ "\n"
-             in ( comp', output )
+    LOAD -> do
+      n <- getVal operand comp lineNo
+      Right ( comp { pc = pc comp + 1, acc = n }, "")
+
+    STORE ->
+      let SymbolOperand s = operand
+          ram' = Map.insert s (acc comp) (ram comp)
+      in Right ( comp{ pc = pc comp + 1, ram = ram' }, "" )
+
+    _ ->
+      let comp' = comp{ pc = pc comp + 1 }
+          output = "step: PC = " ++  (show $ pc comp') ++ "\n"
+      in Right ( comp', output )
+
+
+getVal :: Operand -> Computer -> LineNo -> Either String Integer
+getVal (ValueOperand (Left n)) _ _ = Right n
+getVal (ValueOperand (Right s)) comp lineNo =
+  case Map.lookup s $ ram comp of
+    Just n -> Right n
+    Nothing -> Left $ "\nERROR: Unknown variable: '"
+      ++ s ++ "' at line " ++ show lineNo ++ "\n"
