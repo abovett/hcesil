49eb4d92222b0c04e8a27aca6bc3843e09421682
Andy Bovett <abovett@xemaps.com>
2022-05-10 07:41:07 +0100

    Enhanced command line parser; --help and --version options added.
    Help text not yet written; --maxsteps option present but not yet
    implemented.

---
+++ ParseArgs.hs
@@ -0,0 +1,61 @@
+-- Command line parser
+module ParseArgs
+  ( Params(..)
+  , parseArgs
+  , helpText
+  ) where
+
+import Text.Read
+
+-- A type to hold all parameters after parsing
+data Params =
+  Params
+    { filename :: Maybe String
+    , help :: Bool
+    , version :: Bool
+    , maxSteps :: Maybe Integer
+    }
+  deriving (Show)
+
+-- Parse the command line
+parseArgs :: [String] -> Either String Params
+parseArgs args =
+  let pars =
+        Params
+          { filename = Nothing
+          , help = False
+          , version = False
+          , maxSteps = Nothing
+          }
+   in argParser args pars
+
+-- Command line parsing "loop"
+argParser :: [String] -> Params -> Either String Params
+argParser [] pars = Right pars
+argParser [[]] pars = Left "Empty parameter on command line."
+argParser (a:ra) pars = do
+  (args', pars') <-
+    case () of
+      _
+        | a `elem` ["-h", "--help"] -> Right (ra, pars {help = True})
+        | a `elem` ["-v", "--version"] -> Right (ra, pars {version = True})
+        | a `elem` ["-m", "--maxsteps"] ->
+          (\n -> (tail ra, pars {maxSteps = Just n})) <$> (getPosInt a ra)
+        | (head a) == '-' -> Left $ "Unrecognised option: " ++ a
+        | null . filename $ pars -> Right (ra, pars {filename = Just a})
+        | otherwise -> Left $ "Unexpected argument: " ++ a
+  argParser args' pars'
+  where
+    getPosInt :: String -> [String] -> Either String Integer
+    getPosInt a [] = Left $ "Missing parameter after option: " ++ a
+    getPosInt a ra =
+      case readMaybe $ head ra :: Maybe Integer of
+        Just n ->
+          if n > 0
+            then Right n
+            else Left $ "Option " ++ a ++ " parameter must be > 0"
+        Nothing -> Left $ "Invalid parameter after option: " ++ a
+
+-- Define help text
+-- TODO write help text
+helpText = "Help text not yet written"
--- cesil.hs
+++ cesil.hs
@@ -1,41 +1,47 @@
 -- CESIL interpreter top level
-
-import System.Environment
+-- TODO implement maxsteps
 import Control.Exception
 import Control.Monad.Except
+import Control.Monad.Trans.Maybe
 import Data.List
+import System.Environment
 
 import Compile
 import Execute
+import ParseArgs
 
+progVersion = "hcesil version 1.1.0"
+
+-- Top level of program
 main :: IO ()
-main = handleErrors <=< runExceptT $ do
-  args <- liftIO getArgs
-  filename <- parseArgs args
-  source <- readSource filename
-  (program, dataVals) <- liftEither $ compile source
-  liftIO $ runProgram program dataVals
-
+main =
+  handleErrors <=< runExceptT $ do
+    args <- liftIO getArgs
+    pars <- liftEither $ parseArgs args
+    case () of
+      _
+        | help pars -> liftIO $ putStrLn helpText
+        | version pars -> liftIO $ putStrLn progVersion
+        | otherwise -> do
+          fname <-
+            case filename pars of
+              Nothing -> throwError "No filename specified."
+              Just fname -> return fname
+          source <- readSource fname
+          (program, dataVals) <- liftEither $ compile source
+          liftIO $ runProgram program dataVals
 
 -- Report an error if present.
 handleErrors :: Either String () -> IO ()
 handleErrors (Left err) = putStrLn $ "*** ERROR: " ++ err
 handleErrors (Right _) = return ()
 
-
--- Parse the command line and return the filename or an error
-parseArgs :: [String] -> ExceptT String IO String
-parseArgs args
-  | length args == 0 = throwError "No filename specified."
-  | length (head args) == 0 = throwError "Empty filename."
-  | length args > 1 = throwError "Too many parameters"
-  | otherwise = return $ head args
-
-
 -- Open and read in the source file.
 readSource :: String -> ExceptT String IO String
-readSource filename = ExceptT $ do
-  res <- try $ readFile filename :: IO (Either SomeException String)
-  return $ case res of
-    Left err -> Left . show $ err
-    Right contents -> Right contents
+readSource filename =
+  ExceptT $ do
+    res <- try $ readFile filename :: IO (Either SomeException String)
+    return $
+      case res of
+        Left err -> Left . show $ err
+        Right contents -> Right contents
